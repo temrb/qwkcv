a886a1f408c128716d874f7e1c355191158a0836
Tem <temurbakhriddinov@gmail.com>
2023-11-16 15:38:40 -0600

    integrate dub

--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -15,10 +15,34 @@ export async function POST(request: NextRequest, response: NextResponse) {
 			: 'https://qwkcv.com'
 	}/cv/${encodedLink}`;
 
+	const expirationDate = new Date(600000).getTime(); /* 10 minutes from now */
+
+	const options = {
+		method: 'POST',
+		headers: {
+			Authorization: 'Bearer ' + process.env.DUB_API_KEY,
+			'Content-Type': 'application/json',
+		},
+		body:
+			'{"domain":"qwkcv.com","url" : "' +
+			url +
+			'","expiresAt":' +
+			expirationDate +
+			'}',
+	};
+
 	try {
+		const response = await fetch(
+			'https://api.dub.co/links?projectSlug=qwkcv',
+			options,
+		)
+			.then((response) => response.json())
+			.then((response) => console.log(response))
+			.catch((err) => console.error(err));
+
 		return NextResponse.json({
 			status: 200,
-			link: url,
+			link: response,
 		});
 	} catch (error: any) {
 		return NextResponse.json({
